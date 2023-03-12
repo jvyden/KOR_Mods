95761edcd8a18f37a904706d3f0631ea2008aed4
jvyden <jvyden@jvyden.xyz>
2023-03-12 03:25:28 -0400

    *gasp* the button DOES SOMETHING

--- ModButton/Patches/MainMenuPatches.cs
+++ ModButton/Patches/MainMenuPatches.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Diagnostics.CodeAnalysis;
-using System.Runtime.CompilerServices;
+using System.Reflection;
 using HarmonyLib;
 using JetBrains.Annotations;
 using UnityEngine;
@@ -12,6 +12,11 @@ namespace ModButton.Patches;
 public static class MainMenuPatches
 {
     [CanBeNull] private static GameObject _modsButton;
+
+    private static void HandleModsButtonClick()
+    {
+        Console.WriteLine("REAL");
+    }
 
     [HarmonyPatch(typeof(MainMenu), nameof(MainMenu.Start))]
     [HarmonyPostfix]
@@ -30,8 +35,41 @@ public static class MainMenuPatches
         modsButton.name = "MODS";
         // We don't have TMP available to us from a plugin, so it's time to do some funky things
         GameObject textMesh = GameObject.Find("MODS/Text (TMP)");
-        Component component = textMesh.GetComponentByName("TMPro.TextMeshProUGUI");
-        component.GetType().GetProperty("text")!.SetValue(component, modsButton.name);
+        Component textMeshComponent = textMesh.GetComponentByName("TMPro.TextMeshProUGUI");
+        textMeshComponent
+            .GetType()
+            .GetProperty("text")!
+            .SetValue(textMeshComponent, modsButton.name);
+        
+        // Change button behaviour. Once again, we don't have the button type (UnityEngine.UI.Button) so prepare for hell.
+        // This spaghetti is even worse. I spent hours on this. I'm sorry.
+        Component buttonComponent = modsButton.GetComponentByName("Button");
+        
+        FieldInfo onClickInfo = buttonComponent
+            .GetType()
+            .GetField("m_OnClick", BindingFlags.Instance | BindingFlags.NonPublic);
+        
+        object onClick = onClickInfo!.GetValue(buttonComponent);
+        Type onClickType = onClick.GetType();
+        
+        // Remove all existing events
+        FieldInfo persistentInfo = onClickType.BaseType!.BaseType!
+            .GetField("m_PersistentCalls", BindingFlags.Instance | BindingFlags.NonPublic);
+
+        persistentInfo.SetValue(onClick, Activator.CreateInstance(persistentInfo.FieldType));
+
+        onClickType
+            .GetMethod("RemoveAllListeners", BindingFlags.Instance | BindingFlags.Public)!
+            .Invoke(onClick, Array.Empty<object>());
+
+        // Get the method we're going to invoke with
+        MethodInfo invokeMethod = typeof(MainMenuPatches)
+            .GetMethod(nameof(HandleModsButtonClick), BindingFlags.Static | BindingFlags.NonPublic);
+
+        // Add our own listener
+        onClickType
+            .GetMethod("AddListener", BindingFlags.Instance | BindingFlags.NonPublic)!
+            .Invoke(onClick, new object[]{ null, invokeMethod });
     }
 
     [HarmonyPatch(typeof(MainMenu), nameof(MainMenu.ClickEndless))]
